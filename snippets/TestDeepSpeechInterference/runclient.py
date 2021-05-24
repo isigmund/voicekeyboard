ff2508c9bea38bb52fe12090615a3ecd1e4a0aa4
Ingo Sigmund <ingo@familie-sigmund.eu>
2021-05-24 14:36:31 +0200

    corrected name of runclient script

--- snippets/TestDeepSpeechInterference/runclient.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python3 client.py --modelconfig modelconfig_english.json --audio test1.wav
-
-
---
+++ snippets/TestDeepSpeechInterference/runclient.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+python3 deepspeech_stt.py --modelconfig modelconfig_english.json --audio ../../AudioFiles/one_two_three_four_five.wav
+
+
