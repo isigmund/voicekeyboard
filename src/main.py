d36c689b25bb5ccf650ce6041632bb4d17b0e169
Ingo Sigmund <ingo@familie-sigmund.eu>
2021-06-01 18:16:29 +0200

    initial main file

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def main():   
+  print("main")
+  try:
+    while True:
+      pass
+
+  except KeyboardInterrupt:
+    pass
+
+  finally:
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
